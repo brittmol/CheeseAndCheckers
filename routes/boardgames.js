a4a7411f185806f9260094472935e02060924b0d
Brittany Moliver <britt.moliver@gmail.com>
2021-11-16 16:22:24 -0500

    added to requirements to routes

--- routes/boardgames.js
+++ routes/boardgames.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const { User, BoardGame, GameShelf, Category, Review,  } = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+const { requireAuth } = require('../auth');
 
+const router = express.Router();
 
 module.exports = router;
