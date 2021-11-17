0e914992dbd843f355b3a71546bf7fd7b95eab16
Jeddbasden <jeddbasden2@gmail.com>
2021-11-17 15:29:10 -0600

    heroku setup in database.js

--- config/database.js
+++ config/database.js
@@ -11,17 +11,18 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
+  }
 };
-// production: {
-//   use_env_variable: ‘DATABASE_URL’,
-//   dialect: ‘postgres’,
-//   seederStorage: ‘sequelize’,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
 
 //or
 
